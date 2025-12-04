2d9c2a6e65c80818f4a554d0ad5f0b6c3f79bd4c
Tikkanti <timo.tikkanen@hotmail.com>
2025-12-04 19:31:58 +0200

    Viikkotehtävä 4 valmis

---
+++ Viikkotehtavat/4.Viikkotehtava/ScaffoldAndNavigation/app/src/main/java/com/example/appbarlecture/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.appbarlecture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appbarlecture.ui.screens.ScaffoldApp
+import com.example.appbarlecture.ui.theme.AppbarLectureTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+                AppbarLectureTheme {
+                    ScaffoldApp()
+                }
+            }
+
+        }
+            /*AppbarLectureTheme {
+                Scaffold(
+                    topBar = {
+                        var expanded by remember { mutableStateOf(false) }
+                        TopAppBar(
+                            title = { Text("My App") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            navigationIcon = {
+                                IconButton(onClick = {  }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Menu,
+                                        contentDescription = "Menu icon"
+
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = {expanded = !expanded}) {
+                                    Icon(
+                                        imageVector = Icons.Filled.MoreVert,
+                                        contentDescription = "Open menu"
+
+                                    )
+                                }
+                                DropdownMenu(
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = { Text("Settings") },
+                                        onClick = {}
+
+                                    )
+                                    DropdownMenuItem(
+                                        text = { Text("Info") },
+                                        onClick = {}
+
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }*/
+        }
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppbarLectureTheme {
+        Greeting("Android")
+    }
+}
---
+++ Viikkotehtavat/4.Viikkotehtava/ScaffoldAndNavigation/app/src/main/java/com/example/appbarlecture/ui/screens/SettingsScreen.kt
@@ -0,0 +1,19 @@
+package com.example.appbarlecture.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.appbarlecture.ui.components.ScreenTopBar
+
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Settings",navController) },
+    ) { innerPadding ->
+        Text(text="Settings Screen",modifier = Modifier.padding(innerPadding))
+    }
+}
